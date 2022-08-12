7225ed42b1673c76816dfc401120151073d388ab
awkto <awkto@outlook.com>
2022-08-12 04:11:19 -0400

    working copy of terraform IAC code

--- main.tf
+++ main.tf
@@ -1,23 +1,24 @@
 terraform {
-    required_providers {
-        aws = {
-            source  = "hashicorp/aws"
-            version = "~> 4.25.0 "
-        }
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.25.0 "
     }
+  }
 }
 
+// Create a VPC to use for the rest of our resources
 module "vpc" {
-  source = "terraform-aws-modules/vpc/aws"
-  name                             = "iac-project-vpc"
-  cidr                             = "10.0.0.0/16"
-  azs                              = ["us-east-2a", "us-east-2b"]
-  private_subnets                  = ["10.0.1.0/24", "10.0.2.0/24"]
-  public_subnets                   = ["10.0.101.0/24", "10.0.102.0/24"]
+  source          = "terraform-aws-modules/vpc/aws"
+  name            = "iac-project-vpc"
+  cidr            = "10.0.0.0/16"
+  azs             = ["us-east-2a", "us-east-2b"]
+  private_subnets = ["10.0.1.0/24", "10.0.2.0/24"]
+  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24"]
   #assign_generated_ipv6_cidr_block = true
   #create_database_subnet_group     = true
-  enable_nat_gateway               = true
-  single_nat_gateway               = true
+  enable_nat_gateway = true
+  single_nat_gateway = true
 }
 
 
@@ -28,20 +29,11 @@ resource "aws_key_pair" "altan-key-pair-tf" {
 }
 
 
-// Create Security Group for allowing HTTP / HTTPS traffic
-resource "aws_security_group" "allow_web_traffic" {
-  name        = "iac-project-allow_web_traffic"
+// Create Security Group for public HTTP / HTTPS access
+resource "aws_security_group" "allow_web_traffic_public" {
+  name        = "iac-project-allow_web_traffic_public"
   description = "Allows inbound HTTP / HTTPS connections over the public internet"
   vpc_id      = module.vpc.vpc_id
-
-  //Uncomment to allow public SSH for troubleshooting
-  ingress {
-    description = "Allows inbound SSH connections over the public internet"
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
 
   ingress {
     description = "Allows inbound HTTP connections over the public internet"
@@ -71,6 +63,77 @@ resource "aws_security_group" "allow_web_traffic" {
   }
 }
 
+// Create Security Group for internal HTTP / HTTPS access
+resource "aws_security_group" "allow_web_traffic_internal" {
+  name        = "iac-project-allow_web_traffic_internal"
+  description = "Allows inbound HTTP / HTTPS connections on internal subnets"
+  vpc_id      = module.vpc.vpc_id
+
+  ingress {
+    description = "Allows inbound HTTP connections on internal networks"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["10.0.1.0/24", "10.0.2.0/24", "10.0.101.0/24", "10.0.102.0/24"]
+  }
+
+  ingress {
+    description = "Allows inbound HTTPS connections on internal networks"
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["10.0.1.0/24", "10.0.2.0/24", "10.0.101.0/24", "10.0.102.0/24"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "iac-project-security-group"
+  }
+}
+
+
+
+// Create Security Group for allowing SSH from my public IP (for troubleshooting)
+resource "aws_security_group" "allow_ssh_public" {
+  name        = "iac-project-allow_ssh_public"
+  description = "Allows inbound SSH port 22 over internet"
+  vpc_id      = module.vpc.vpc_id
+
+  ingress {
+    description = "Allows inbound SSH connections over the public internet"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["184.148.169.106/32"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "iac-project-security-group"
+  }
+}
+
+//Create S3 bucket for logs
+resource "aws_s3_bucket" "iac-project-logs" {
+  bucket = "iac-project-logs"
+  tags = {
+    Name        = "iac-project-logs"
+    Environment = "Dev"
+  }
+}
+
 //Create S3 bucket to source NGINX content
 resource "aws_s3_bucket" "iac-project-nginx-configfiles" {
   bucket = "iac-project-nginx-configfiles"
@@ -80,15 +143,26 @@ resource "aws_s3_bucket" "iac-project-nginx-configfiles" {
   }
 }
 
-# resource "aws_s3_bucket_acl" "example" {
-#   bucket = aws_s3_bucket.b.id
-#   acl    = "private"
-# }
+# Upload webpage file to S3
+resource "aws_s3_object" "index.html" {
+  bucket = aws_s3_bucket.iac-project-nginx-configfiles.bucket
+  key    = "index.html"
+  source = "./index.html"
+  #source_hash = filemd5(local.object_source)
+}
+
+# Upload media file to S3
+resource "aws_s3_object" "altan.jpg" {
+  bucket = aws_s3_bucket.iac-project-nginx-configfiles.bucket
+  key    = "altan.jpg"
+  source = "./altan.jpg"
+  #source_hash = filemd5(local.object_source)
+}
 
 
 //Allowing EC2 instance access to S3 bucket PART 1 - Create IAM Role
-resource "aws_iam_role" "s3-downloader" {
-  name = "s3-downloader"
+resource "aws_iam_role" "s3-iam-role" {
+  name = "s3-iam-role"
 
   assume_role_policy = <<EOF
 {
@@ -107,20 +181,20 @@ resource "aws_iam_role" "s3-downloader" {
 EOF
 
   tags = {
-      tag-key = "tag-value"
+    tag-key = "tag-value"
   }
 }
 
 //Allowing EC2 instance access to S3 bucket PART 2 - Create an IAM Instance Profile for EC2
-resource "aws_iam_instance_profile" "s3-downloader" {
-  name = "s3-downloader"
-  role = "${aws_iam_role.s3-downloader.name}"
+resource "aws_iam_instance_profile" "s3-instance-profile" {
+  name = "s3-instance-profile"
+  role = aws_iam_role.s3-iam-role.name
 }
 
 //Allowing EC2 instance access to S3 bucket PART 3 - Create Policy
-resource "aws_iam_role_policy" "s3-downloader" {
-  name = "s3-downloader"
-  role = "${aws_iam_role.s3-downloader.id}"
+resource "aws_iam_role_policy" "s3-policy" {
+  name = "s3-policy"
+  role = aws_iam_role.s3-iam-role.id
 
   policy = <<EOF
 {
@@ -139,92 +213,91 @@ EOF
 }
 
 // Autoscaling Group Part 1 - Create Launch Configuration
-resource "aws_launch_configuration" "iac-launch-config" {
-  name          = "web_config"
-  image_id      = "ami-090fa75af13c156b4"
-  instance_type = "t2.micro"
-  key_name      = "altan-key-pair-tf"
-  security_groups = [aws_security_group.allow_web_traffic.id]
-  iam_instance_profile = aws_iam_instance_profile.s3-downloader.id
-  user_data     = "IyEvYmluL2Jhc2gKYW1hem9uLWxpbnV4LWV4dHJhcyBpbnN0YWxsIG5naW54MQphd3MgczMgY3AgczM6Ly9pYWMtcHJvamVjdC1uZ2lueC1jb25maWdmaWxlcy90ZXN0ZmlsZSAvaG9tZS9lYzItdXNlci8K"
+resource "aws_launch_configuration" "iac-as-launch-config" {
+  name                 = "iac_as_launch-config"
+  image_id             = "ami-051dfed8f67f095f5"
+  instance_type        = "t2.micro"
+  key_name             = "altan-key-pair-tf"
+  security_groups      = [aws_security_group.allow_web_traffic_public.id, aws_security_group.allow_ssh_public.id]
+  iam_instance_profile = aws_iam_instance_profile.s3-instance-profile.id
+  user_data            = "IyEvYmluL2Jhc2gKYW1hem9uLWxpbnV4LWV4dHJhcyBpbnN0YWxsIG5naW54MQpzeXN0ZW1jdGwgZW5hYmxlIG5naW54CnN5c3RlbWN0bCBzdGFydCBuZ2lueApta2RpciAvaG9tZS9lYzItdXNlci93ZWJwYWdlCmNwIC1SIC91c3Ivc2hhcmUvbmdpbngvaHRtbCAvdXNyL3NoYXJlL25naW54L2lhYy1wcm9qZWN0CmF3cyBzMyBjcCBzMzovL2lhYy1wcm9qZWN0LW5naW54LWNvbmZpZ2ZpbGVzL2luZGV4Lmh0bWwgL3Vzci9zaGFyZS9uZ2lueC9pYWMtcHJvamVjdC9pbmRleC5odG1sCmF3cyBzMyBjcCBzMzovL2lhYy1wcm9qZWN0LW5naW54LWNvbmZpZ2ZpbGVzL2FsdGFuLmpwZyAvdXNyL3NoYXJlL25naW54L2lhYy1wcm9qZWN0L2FsdGFuLmpwZwpzeXN0ZW1jdGwgcmVsb2FkIG5naW54Cg=="
 }
 
 
 // Autoscaling Group Part 2 - Create Placement Group
-resource "aws_placement_group" "iac-placement-group" {
-  name     = "iac-autoscaling-group"
-  strategy = "cluster"
+resource "aws_placement_group" "iac-as-placement-group" {
+  name     = "iac-as-placement-group"
+  strategy = "partition"
 }
 
 // Autoscaling Group Part 3 - Create Autoscaling Group
-resource "aws_autoscaling_group" "iac-autoscaling-group" {
+resource "aws_autoscaling_group" "iac-as-autoscaling-group" {
   name                      = "iac-autoscaling-group"
-  max_size                  = 5
-  min_size                  = 2
+  max_size                  = 3
+  min_size                  = 1
   health_check_grace_period = 300
   health_check_type         = "ELB"
-  desired_capacity          = 4
+  desired_capacity          = 2
   force_delete              = true
-  placement_group           = aws_placement_group.iac-placement-group.id
-  launch_configuration      = aws_launch_configuration.iac-launch-config.name
-  # vpc_zone_identifier       = [aws_subnet.example1.id, aws_subnet.example2.id]          //FIX THIS
-  # vpc_zone_identifier       = ["${data.aws_subnet_ids.all.ids}"]
+  placement_group           = aws_placement_group.iac-as-placement-group.id
+  launch_configuration      = aws_launch_configuration.iac-as-launch-config.id
   vpc_zone_identifier       = module.vpc.public_subnets
 
-  initial_lifecycle_hook {
-    name                 = "foobar"
-    default_result       = "CONTINUE"
-    heartbeat_timeout    = 2000
-    lifecycle_transition = "autoscaling:EC2_INSTANCE_LAUNCHING"
-
-    notification_metadata = <<EOF
-{
-  "foo": "bar"
-}
-EOF
-
-    notification_target_arn = "arn:aws:sqs:us-east-1:444455556666:queue1*"
-    role_arn                = "arn:aws:iam::123456789012:role/S3Access"
-  }
-
-  tag {
-    key                 = "foo"
-    value               = "bar"
-    propagate_at_launch = true
-  }
-
-  timeouts {
-    delete = "15m"
-  }
-
-  tag {
-    key                 = "lorem"
-    value               = "ipsum"
-    propagate_at_launch = false
-  }
 }
 
+// Autoscaling Group Part 4 - Create an increment policy
+resource "aws_autoscaling_policy" "iac-as-policy-increase" {
+  autoscaling_group_name = aws_autoscaling_group.iac-as-autoscaling-group.name
+  name                   = "iac-as-policy-increase"
+  policy_type            = "SimpleScaling"
+  cooldown               = 300
+  scaling_adjustment     = 1
+  adjustment_type        = "ChangeInCapacity"
+}
+
+// Autoscaling Group Part 5 - Create an reduction policy
+resource "aws_autoscaling_policy" "iac-as-policy-decrease" {
+  autoscaling_group_name = aws_autoscaling_group.iac-as-autoscaling-group.name
+  name                   = "iac-as-policy-decrease"
+  policy_type            = "SimpleScaling"
+  cooldown               = 300
+  scaling_adjustment     = -1
+  adjustment_type        = "ChangeInCapacity"
+}
+
+// Load Balancer Part 1 - Load Balancer
+resource "aws_lb" "iac_loadbalancer" {
+  name                       = "iac-loadbalancer"
+  internal                   = false
+  load_balancer_type         = "application"
+  security_groups            = [aws_security_group.allow_web_traffic_public.id]
+  subnets                    = module.vpc.public_subnets
+  enable_deletion_protection = true
+
+}
+
+// Load Balancer Part 2 - Target Group
+resource "aws_lb_target_group" "iac-lb-target-group" {
+  name     = "iac-lb-target-group"
+  port     = 80
+  protocol = "HTTP"
+  vpc_id   = module.vpc.vpc_id
+}
+
+// Load Balancer Part 3 - Listener
+resource "aws_lb_listener" "iac-lb-listener" {
+  load_balancer_arn = aws_lb.iac_loadbalancer.arn
+  port              = "80"
+  protocol          = "HTTP"
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.iac-lb-target-group.arn
+  }
+}
 
 
 
-# // Create an EC2 Instance
-# resource "aws_instance" "web_app_server_01" {
-#   ami           = "ami-051dfed8f67f095f5"
-#   instance_type = "t2.micro"
-#   key_name      = "altan-key-pair-tf"
-#   tags = {
-#     Name = "IaC-Assignment-01"
-#   }
-# }
 
-# // Create a second EC2 Instance
-# resource "aws_instance" "web_app_server_02" {
-#   ami           = "ami-051dfed8f67f095f5"
-#   instance_type = "t2.micro"
-#   key_name      = "altan-key-pair-tf"
-#   tags = {
-#     Name = "IaC-Assignment-02"
-#   }
-# }
-
-
+//TODO
+//Integrate ASG with ELB target group
